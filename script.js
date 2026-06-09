81f940bb72d41dacd3f73fa40618a07a3e208064
marijavujanovic2008-max <marijavujanovic2008@gmail.com>
2026-06-09 23:14:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("theme").onclick=()=>document.body.classList.toggle("dark");
+function check(x){document.getElementById("r").innerText=x=="b"?"Tačno!":"Netačno, odgovor je korektor."; }
